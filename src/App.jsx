5bc1936fca09a723cf6f85b7fd72b6f18cc724ce
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-03 10:31:14 +0530

    Create layout of the PersonalInfoForm.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import PersonalInfo from "./components/PersonalInfo";
 import Profile from "./components/Profile";
 import ExperienceSection from "./components/ExperienceSection";
 
+import PersonalInfoForm from "./components/PersonalInfoFrom";
+
 export default function App() {
 	return (
 		<div className="flex justify-between">
-			<div className="flex-1/2 bg-pink-200">Edit side</div>
+			<div className="flex-1/2 bg-pink-200 p-2 flex flex-col gap-1 min-h-screen">
+				<PersonalInfoForm />
+			</div>
 
 
 			<div className="flex-1/2 border-2 border-b-blue-950 min-h-screen bg-purple-600 p-2 flex flex-col gap-1">
