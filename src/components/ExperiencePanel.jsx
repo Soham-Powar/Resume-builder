a76ac0f31bac3052fed0a5c0a683fd0587a8097c
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-07 10:18:51 +0530

    Add a save button to update the form.

--- src/components/ExperiencePanel.jsx
+++ src/components/ExperiencePanel.jsx
@@ -11,7 +11,11 @@ export default function ExperiencePanel({ setExperiences, experiences }) {
 			<FormHeading section="experiences" />
 
 			{formShown >= 0 ? (
-				<ExperienceForm experiences={experiences} setExperiences={setExperiences} expID={formShown} />
+				<>
+					<ExperienceForm experiences={experiences} setExperiences={setExperiences} expID={formShown} />
+					<button onClick={() => setFormShown(-1)} className="bg-[#187FF5] text-white font-bold py-1.5 px-2.5 rounded-lg ml-[40%] hover:bg-[#187ff5c4]">Save</button>
+				</>
+
 			) : (
 				<ul className="flex flex-col gap-1.5 js-experiences">
 					{experiences.map(experience => {
